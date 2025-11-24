8c1b0105893b866f8f62cc58fbf74d5121389180
mewmix <him@Alexanders-MacBook-Pro.local>
2025-11-23 19:48:09 -0800

    Enable AndroidX and add terminal icon assets

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -71,6 +71,7 @@ dependencies {
     implementation("androidx.activity:activity-compose:1.8.2")
     implementation(platform("androidx.compose:compose-bom:2024.02.01"))
     implementation("androidx.compose.ui:ui")
+    implementation("androidx.compose.foundation:foundation")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     testImplementation("junit:junit:4.13.2")
